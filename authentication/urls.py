30872c4212dfc5907322de26f9445f34c1253b39
showhack <hellshift0514@gmail.com>
2024-06-11 14:29:24 -0400

    delete test function

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-from . import views
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
-    path("test/", views.test),
 ]
